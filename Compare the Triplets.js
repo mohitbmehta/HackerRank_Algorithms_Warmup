2b588b9f9f2827347b6dfcbf3f46c7096e60e1fd
Mohit Mehta <mohitbmehta07@gmail.com>
2017-04-25 17:58:12 -0700

    Add files via upload

---
+++ Compare the Triplets.js
@@ -0,0 +1,37 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var a0_temp = readLine().split(' ');
+    var a0 = parseInt(a0_temp[0]);
+    var a1 = parseInt(a0_temp[1]);
+    var a2 = parseInt(a0_temp[2]);
+    var b0_temp = readLine().split(' ');
+    var b0 = parseInt(b0_temp[0]);
+    var b1 = parseInt(b0_temp[1]);
+    var b2 = parseInt(b0_temp[2]);
+    // Write Your Code Here
+    var pAlice = ((a0>b0)?1:0)+ ((a1>b1)?1:0)+ ((a2>b2)?1:0);
+    var pBob = ((a0<b0)?1:0)+ ((a1<b1)?1:0)+ ((a2<b2)?1:0);
+    console.log(pAlice+" "+pBob);
+
+}
